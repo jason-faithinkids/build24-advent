e38aae0e693d39988f3b9d87524d493000f77224
Andy Geers <andy@geero.net>
2024-10-12 18:18:08 +0100

    Better touch support and flip with double tap

--- js/stickerbook.js
+++ js/stickerbook.js
@@ -2,6 +2,7 @@ function initialiseStickerbook() {
 	let draggingElement = null;
     let offsetX, offsetY;
     let startedDrag = false;
+	var tapped=false;
 
     let startTouch = function(_this, e) {
     	draggingElement = _this;
@@ -37,15 +38,31 @@ function initialiseStickerbook() {
     });
     $('.sticker').on('touchstart', function(e) {    	
         startTouch(this, e.touches[0]);
+
+        if(!tapped){ //if tap is not set, set up single tap
+	      tapped=setTimeout(function(){
+	          tapped=null
+	          //insert things you want to do when single tapped
+	      },300);   //wait 300ms then run single click code
+	    } else {    //tapped within 300ms of last tap. double tap
+	      clearTimeout(tapped); //stop single tap callback
+	      tapped=null
+	      //insert things you want to do when double tapped
+	      $(this).toggleClass('flipped');
+	    }
     });
 
     // Mouse move event to handle dragging
     $(document).on('mousemove', function(e) {    	
         moveTouch(e);
     });
-    $(document).on('touchmove', function(e) {    	
-        moveTouch(e.touches[0]);
+    $(document).on('touchmove', function(e) {    	    	
+        moveTouch(e.touches[0]);       
     });
+
+    $('.sticker').on('dblclick', function(e) {    	
+    	$(this).toggleClass('flipped');
+    });    
 
     // Mouse up event to stop dragging
     $(document).on('mouseup touchend', function() {
