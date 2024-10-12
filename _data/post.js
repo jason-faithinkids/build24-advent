2c140400161af2690accba688850200831917084
Alasdair Munday <alasdairmunday@gmail.com>
2024-10-12 17:37:52 +0100

    nl2br

--- _data/post.js
+++ _data/post.js
@@ -43,8 +43,6 @@ const randomColour = () => Math.floor(Math.random()*16777215).toString(16);
 // process WordPress posts
 module.exports = async function() {
   // fetch all pages of posts
-  console.log('>>>>>>>>>>>>>>')
   return wpPosts();
-
 };
 
