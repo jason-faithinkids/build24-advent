15a33ed868c536fe4a31e3f6641d32a49966dc9a
Alasdair Munday <alasdair.munday@googlemail.com>
2024-11-17 19:34:29 +0000

    fix inactive and used day functionality (#14)
    Co-authored-by: Alasdair Munday <alasdairmunday@gmail.com>

--- js/advent.js
+++ js/advent.js
@@ -64,6 +64,24 @@ function getEndTransition(targetPage, stencil) {
 }
 
 function initialiseAdvent() {
+	// Get the current day of the month
+	var today = new Date().getDate();
+	// Select elements with IDs starting with "day-" and convert them to an array
+	var dayElements = $("[id^=day-]").toArray();
+	// Iterate through the array and add the class to elements with higher day numbers
+	dayElements.forEach(function(element) {
+		var dayNumber = parseInt(element.id.replace("day-", ""));
+		console.log("sticker-" + dayNumber + "-x")
+		var savedStateX = window.localStorage.getItem("sticker-" + dayNumber + "-x");
+		console.log(savedStateX)
+		if (dayNumber > today) {
+			$(element).addClass("lego-brick-inactive").prop("disabled", true);
+		} else if(savedStateX != null) {
+			$(element).addClass("lego-brick-used");
+		}
+	});
+
+
 	// $('#zoom-in').click(function() { zoomIn(); });
 	// $('#zoom-out').click(function() { zoomOut(); });
 	
